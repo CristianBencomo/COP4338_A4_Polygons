25b118ec0f97e9d49a002ef44240b35bf9670bf7
Cristian Bencomo <cristian.bencomo@outlook.com>
2021-03-14 16:11:58 -0400

    0.0.3: input strtok bug

--- Polygons.c
+++ Polygons.c
@@ -52,36 +52,53 @@ char *dynamicScan(char *str)
 }
 
 // Function that splits input into words
-void splitstr(char *str, char *command, char *parameters)
+void splitstr(char *str, char *command)
 {
     char *token = strtok(str, " \0"); 
     strcpy(command, token);
-    token = strtok(NULL, " \0");
-
-
-    if(token != NULL)
-    {
-        strcpy(parameters, token);
-    }
 }
 
 // adding a polygon to the list
 void add(char *str)
 {
-    polyList[polygonCount].numberOfVertices = 0;
+    char *token = strtok(str, " \0");
+    token = strtok(NULL, " \0"); //skipping first token which is the command
+
     Polygon *currentPolygon = &polyList[polygonCount];
+    currentPolygon->numberOfVertices = 0;
     currentPolygon->shiftDirection = NONE;
     currentPolygon->vertexList = malloc(1 * sizeof(Polygon));
 
-    for(int i=0; str[i] != '\0' || str[i+1] != '\0'; i+=2) // Loops through every character in the string
+    int x, y, inputCounter = 0;
+    printf("Before while loop\n");
+    while(token != NULL)
     {
-        
-        printf("%d", i);
-        currentPolygon->vertexList = realloc(currentPolygon->vertexList, (i+1) * sizeof(Polygon)); // Allocate memory for new vertex
-        currentPolygon->vertexList[currentPolygon->numberOfVertices].x = str[i] - '0'; // X
-        currentPolygon->vertexList[currentPolygon->numberOfVertices].y = str[i+1] - '0'; // Y
-        currentPolygon->numberOfVertices++;
+        printf("In While loop\n");
+        if(inputCounter % 2 == 0)
+        {
+            x = atoi(token);
+            printf("in if\n");
+        }
+        else
+        {
+            printf("in else\n");
+            y = atoi(token);
+            currentPolygon->vertexList = realloc(currentPolygon->vertexList, (inputCounter + 1) * sizeof(Polygon)); // Allocate memory for new vertex
+            currentPolygon->vertexList[currentPolygon->numberOfVertices].x = x; // X
+            currentPolygon->vertexList[currentPolygon->numberOfVertices].y = y; // Y
+            currentPolygon->numberOfVertices++;
+            printf("X=%d Y=%d\n",currentPolygon->vertexList[currentPolygon->numberOfVertices].x, currentPolygon->vertexList[currentPolygon->numberOfVertices].y );
+        }
+        inputCounter++;
     }
+
+    // for(int i=0; str[i] != '\0' || str[i+1] != '\0'; i+=2) // Loops through every character in the string
+    // {
+    //     currentPolygon->vertexList = realloc(currentPolygon->vertexList, (i+1) * sizeof(Polygon)); // Allocate memory for new vertex
+    //     currentPolygon->vertexList[currentPolygon->numberOfVertices].x = str[i] - '0'; // X
+    //     currentPolygon->vertexList[currentPolygon->numberOfVertices].y = str[i+1] - '0'; // Y
+    //     currentPolygon->numberOfVertices++;
+    // }
     polygonCount++;
 }
 
@@ -93,24 +110,44 @@ void summary(int polygonCount)
     {
         Polygon currentPolygon = polyList[i];
         int numVertices = currentPolygon.numberOfVertices;
-        int centroid_x = 0, centroid_y = 0; 
+        float centroid_x = 0, centroid_y = 0; 
         for(int j = 0; j <numVertices; j++) // For each vertex
         {   
             centroid_x += currentPolygon.vertexList[j].x; // add current x
             centroid_y += currentPolygon.vertexList[j].y; // add current y
         }
 
+        printf("Centroids x= %d    y=%d\n", centroid_x, centroid_y);
         centroid_x /= numVertices; // divide for centroid final result
         centroid_y /= numVertices; // divide for centroid final result
         
         //Printing 
         printf("Polygon #%d:\n", i+1);
+        printf("direction: %d\n", currentPolygon.shiftDirection);
         printf("Number of vertices = %d\nCentroid: X=%d Y=%d\n\n", numVertices,centroid_x, centroid_y);
+        
     }
 }
 
-void turn()
+void turn(char *str)
 {
+    char *token = strtok(str, " \0\n");
+    printf("token %s\n", token);
+    char *polygon_idx = malloc(sizeof(token));
+    strcpy(polygon_idx, token); //Saving polygon index parameter
+    printf("polygon_idx = %s\n", polygon_idx);
+
+    Polygon *currentPolygon = &polyList[atoi(polygon_idx)]; //creating a pointer to the 
+
+    token = strtok(NULL, " ");
+    printf("token = %s\n", token);
+    int direction = atoi(token);
+
+    // printf("direction_int: %d\n", direction);
+    // currentPolygon->shiftDirection = direction;
+
+    free(polygon_idx);
+    // free(direction);
 
 }
 
@@ -131,12 +168,13 @@ int main(int argc, char const *argv[])
         char * input;
         char *str;
         input = dynamicScan(str);
+        str = realloc(str, strlen(sizeof(input)));
+        strcpy(str,input);
+        // free(str);
 
         // separating command and parameters
         char *command = malloc(COMMAND_LENGTH*sizeof(char));
-        char *parameters = malloc((INPUT_LENGTH - COMMAND_LENGTH) *sizeof(char));
-        splitstr(input, command, parameters);
-        free(input); // free memory from input
+        splitstr(input, command);
         
         if(strcasecmp(command, "add") == 0) // Add command
         {
@@ -145,8 +183,8 @@ int main(int argc, char const *argv[])
                 printf("1000 polygons have been saved, no more polygons can be added\nPlease use a different command\n");
                 continue;
             }
-            add(parameters);
-            // Add printf?
+            add(str);
+            printf("Polygon added\n");
         }
 
         else if(strcasecmp(command, "summary") == 0) //summary command
@@ -156,7 +194,8 @@ int main(int argc, char const *argv[])
 
         else if(strcasecmp(command, "turn") == 0) // turn command
         {
-
+            turn(input);
+            printf("Polygon direction changed\n");
         }
 
         else if(strcasecmp(command, "shift") == 0) // shift command
@@ -177,7 +216,7 @@ int main(int argc, char const *argv[])
 
         // free allocated space
         free(command);
-        free(parameters);
+        free(input);
         
     } while (!wantToExit);    
     
